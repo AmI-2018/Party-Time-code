86e630e6fc5c70171854fc4938740ebef4cdf99e
alessio-zamparelli <alessio.zamparelli@studenti.polito.it>
2018-05-08 10:32:57 +0200

    working on stored music

--- server/DBoperator.py
+++ server/DBoperator.py
@@ -1,6 +1,9 @@
 import sqlite3
 import logging
 import sys
+import os
+import glob
+from time import sleep
 
 path = "./tmpDB"
 
@@ -26,21 +29,81 @@ DROP TABLE IF EXISTS `musicDB`;
 (
   `id` INTEGER PRIMARY KEY AUTOINCREMENT,
   `title` varchar(200) NOT NULL,
-  `kind` varchar(200) NOT NULL
+  `kind` varchar(200) NOT NULL,
+  `location` varchar(200) NOT NULL 
 );
      
         """
         cur.executescript(query)
         con.commit()
-        cur.execute("select  * from musicDB")
-        rows = cur.fetchall()
-        print("risultati " + str(rows))
+        #cur.execute("select  * from musicDB")
+        #rows = cur.fetchall()
+        #print("risultati " + str(rows))
+        cur.close()
+        con.close()
     except sqlite3.DataError as DataErr:
         print("errore di creazione table " + DataErr.args[0])
     except sqlite3.DatabaseError as DBerror:
         print("errore nell'apertura del db " + DBerror.args[0])
         sys.exit(1)
 
+def newTrack(trackName, trackPath, trackKind):
+    try:
+        con = sqlite3.connect(path)
+        cur = con.cursor()
+        query = """INSERT INTO `musicDB` (`title`, `kind`, `location`) VALUES (?,?,?)"""
+        cur.execute(query, (trackName, trackPath, trackKind))
+        con.commit()
+        # cur.execute("select  * from musicDB")
+        # rows = cur.fetchall()
+        # print("risultati " + str(rows))
+        cur.close()
+        con.close()
+    except sqlite3.DataError as DataErr:
+        print("errore di creazione table " + DataErr.args[0])
+    except sqlite3.DatabaseError as DBerror:
+        print("errore nell'apertura del db " + DBerror.args[0])
+        sys.exit(1)
+def showAllMusic():
+    try:
+        print("show all music")
+        con = sqlite3.connect(path)
+        cur = con.cursor()
+        query = """select `title`, `kind`, `location` from `musicDB`"""
+        cur.execute(query)
+        rows = cur.fetchall()
+        print("risultati")
+        for row in rows:
+            print(row)
+        cur.close()
+        con.close()
+    except sqlite3.DataError as DataErr:
+        print("errore di creazione table " + DataErr.args[0])
+    except sqlite3.DatabaseError as DBerror:
+        print("errore nell'apertura del db " + DBerror.args[0])
+        sys.exit(1)
+
+def importMusic():
+
+    for dirpath, dirnames, filenames in os.walk("./music"):
+        #print("dirpath dir: " +   dirpath)
+        for file, dir in (filenames, dirnames):
+            print("file: " + str(file))
+            sleep(5)
+            print("dir: " + str(dir))
+            #newTrack(file, dirpath, os.path.join(dirpath, file))
+            #print(file)
+    showAllMusic()
+
+
+
+
+"""
+    for kind in os.listdir('./music'):
+        res = glob.glob(kind + "*")
+        print(res)
+"""
 
 if __name__ == '__main__':
     DBinit()
+    importMusic()
