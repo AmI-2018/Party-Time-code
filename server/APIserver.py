abce3ccd82acf5b48d6e6af7e69499413e517d0e
MatteoZhang <s223012@studenti.polito.it>
2018-05-28 14:55:01 +0200

    api

--- server/APIserver.py
+++ server/APIserver.py
@@ -1,5 +1,12 @@
+
 class APIserver:
     def __init__(self):
+
+        from flask import Flask, redirect, url_for, render_template, session, request, jsonify
+        import DBoperator as db
+
+        import logging
+
         pass
 
     def ciao(self):
