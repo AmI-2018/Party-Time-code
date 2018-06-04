af014c30b9a29ee7d9b6c8d68e0d64e81657ad7d
MatteoZhang <s223012@studenti.polito.it>
2018-06-04 16:52:47 +0200

    vote trial

--- party2018_Bot/vote.py
+++ party2018_Bot/vote.py
@@ -1,3 +1,10 @@
+"""
+
+this is just an idea of the voting system which should be a proportional
+compilation of a playlist
+
+"""
+
 
 print("hello")
 genres = ["pop", "rock", "classic"]
@@ -9,6 +16,7 @@ total_users = users["pop"]+users["rock"]+users["classic"]
 print("total users are " + str(total_users))
 users_percentage = {"pop": users["pop"]/total_users, "rock": users["rock"]/total_users,
                     "classic": users["classic"]/total_users}
+print("songs percentage")
 print(str(int(users_percentage["pop"]*playlist_length)) + " " +
       str(int(users_percentage["rock"]*playlist_length)) + " " +
       str(int(users_percentage["classic"]*playlist_length)))
