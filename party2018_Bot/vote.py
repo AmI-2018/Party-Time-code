d688318811ec41cc87ead516fd93f5a7725be8d4
MatteoZhang <s223012@studenti.polito.it>
2018-06-04 17:08:18 +0200

    vote trial

--- party2018_Bot/vote.py
+++ party2018_Bot/vote.py
@@ -10,13 +10,13 @@ print("hello")
 genres = ["pop", "rock", "classic"]
 users = {"pop": 6, "rock": 9, "classic": 1}
 print("write the length of the playlist u want to create:...")
-playlist_length = float(input())
+playlist_length = int(input())
 # try a number smaller , bigger and equal to total users
 total_users = users["pop"]+users["rock"]+users["classic"]
 print("total users are " + str(total_users))
-users_percentage = {"pop": users["pop"]/total_users, "rock": users["rock"]/total_users,
-                    "classic": users["classic"]/total_users}
-print("songs percentage")
-print(str(int(users_percentage["pop"]*playlist_length)) + " " +
-      str(int(users_percentage["rock"]*playlist_length)) + " " +
-      str(int(users_percentage["classic"]*playlist_length)))
+users_percentage = {"pop": int(users["pop"]/total_users), "rock": int(users["rock"]/total_users),
+                    "classic": int(users["classic"]/total_users)}
+print("songs percentage multiply by "+str(total_users))
+print((str(users_percentage["pop"]*playlist_length)) + " " +
+      (str(users_percentage["rock"]*playlist_length)) + " " +
+      (str(users_percentage["classic"]*playlist_length)))
