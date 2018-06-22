f4a965148c51770e4749c439dc0feffa136a7525
LucaGonella <lucagonella1996@gmail.com>
2018-06-22 18:10:10 +0200

    deleted useless code

--- server/music_play.py
+++ server/music_play.py
@@ -1,5 +1,6 @@
 from pygame import mixer # Load the required library
 
 mixer.init()
-mixer.music.load('playlist\song.mp3')
+mixer.music.load("playlist\song.mp3")
 mixer.music.play()
+
--- server/play_music.py
+++ server/play_music.py
@@ -2,6 +2,10 @@ import os, random, time, sys
 import glob
 from pygame import mixer # Load the required library
 
+import mpg123
+from pygame import mixer # Load the required library
+from mpg123 import Mpg123
+
 mp3s = []
 mediapath = "playlist"
 
@@ -13,17 +17,24 @@ for path, directory, element in os.walk(mediapath, False):
     for i in range(0, len(tmparray)):
         if (tmparray[i][-3:] == "mp3" and tmparray[i][:1] != "."):
             mp3s.append(tmparray[i])
+            print(mp3s[i])
         else:
             print("Unuseable:", tmparray[i])
 
     print("Loaded " + str(len(mp3s)) + " files, of " + str(len(element)) + " total")
 
-print(mp3s)
+
 random.shuffle(mp3s)
 
-#prima opzione
-for i in range(0, len(tmparray)):
+"""
+for i in range(0, len(tmparray)-1):
     print(mp3s[i])
     os.system("mpg123 '" + mediapath + mp3s[i] + "'")
     time.sleep(1)
+"""
 
+for k in range(0, len(tmparray)-1):
+    print(mp3s[k])
+    mixer.init()
+    mixer.music.load(mp3s[k])
+    mixer.music.play()
