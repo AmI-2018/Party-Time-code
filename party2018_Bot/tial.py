8d9bb746d748a144452e05aab096782f95416af9
matteo <s223012@studenti.polito.it>
2018-07-09 17:54:22 +0200

    voting functions

--- party2018_Bot/tial.py
+++ party2018_Bot/tial.py
@@ -1,21 +1,26 @@
+"""
+prototype used for vote_function
 
-def vote(roomName):
-    N = 10  # number of songs per playlist
+"""
 
-    kinds = [rock, pop, jazz]  # initialize list
+import operator
+
+def vote():
+    N = 100  # number of songs per playlist
+
+    kinds = ["rock", "pop", "jazz"]  # initialize list
     print(kinds)
 
-    user_per_genre = []  # initialize list
+    user_per_genre = [9,3,1]  # initialize list
     playlist = {}  # empty dictionary
-    total_users = DBoperator.countTotalUser()
+    total_users = 3+9+1
 
-    for i in range(0, total_users):
-        user_per_genre[i] = DBoperator.countUser(kinds[i])
-
-    for i in range(0, total_users):
-        playlist[kinds[i]] = float(user_per_genre[i]) / float(total_users) * N
-    print(playlist)
-    playlistByRoom{roomName }
+    for i in range(0, 3):
+        playlist[kinds[i]] = int(float(user_per_genre[i]) / float(total_users) * N)
 
     return playlist
 
+if __name__ == '__main__':
+    playlist=vote()
+    sorted_list = sorted(playlist.items(), key=operator.itemgetter(1),reverse=True)
+    print(sorted_list)
--- server/vote_function.py
+++ server/vote_function.py
@@ -1,28 +1,25 @@
 import DBoperator as DB
 
-def vote(roomName):
+def vote():
 
-    N=10 #number of songs per playlist
-    rows=DB.getKindsOfMusicAndCount()
+    N=20 #number of songs per playlist
 
     kinds=[rock,pop,jazz]  #initialize list
-    print(kinds)
     user_per_genre=[]  #initialize list
     playlist={} #empty dictionary
 
-    for kind,count in rows :
-        kinds.append(kind)
+    total_users = DB.countTotalUser() #number of users in a specific room
 
-    print(kinds)
-    total_users = DB.countTotalUser()
+    for i in range(0,3):
+        user_per_genre[i] = DB.countUser(kinds[i]) #users of kind[i] present in a specific room
 
-    for i in range(0,total_users):
-        user_per_genre[i] = DB.countUser(kinds[i])
-
-    for i in range(0,total_users):
-        playlist[kinds[i]] = float(user_per_genre[i])/float(total_users)*N
+    for i in range(0,3):
+        playlist[kinds[i]] = int(float(user_per_genre[i])/float(total_users)*N)
+        #with the playlist now we have to play songs based on the playlist
 
     return playlist
+
+#create a folder with playlist characteristics and pass the directory to ftp.py
 
 
 
