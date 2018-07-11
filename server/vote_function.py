7e31994de2724892f090882fea3fb19d8e86d075
MatteoZhang <s223012@studenti.polito.it>
2018-07-11 11:39:40 +0200

    voting

--- server/vote_function.py
+++ server/vote_function.py
@@ -10,12 +10,26 @@ def vote():
     for i in range (0,3):
         users_per_gen [kinds[i]] = db.countUserInRoomByGenre(kinds[i])
         print(users_per_gen)
-    for j in range (0,len(rooms)):
-        for i in range (0,len(kinds)):
-           playlist[len(rooms)][len(kinds)]=
+    playlist={
+        rooms[0]: [N, 0, 0],
+        rooms[1]: [0, N, 0],
+    }
+    id_songs=[[],[]]
+    for i in range (0,N):
+        id_songs[0].append(i+1)
+        id_songs[1].append(i+66)
+    print(playlist)
+    print(id_songs)
+    return id_songs
 
 if __name__ == '__main__':
-    vote()
+    playlistids=vote()
+    """
+    
+    the vote function return a playlist with each rows indicating a room and each column indicating
+    an id of a song present in the database  
+    
+    """
 
 
 
