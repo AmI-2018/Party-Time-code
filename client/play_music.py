8564d0a9de7ee492dc412b55c5c4de8df1d7c50b
alessio-zamparelli <alessio.zamparelli@studenti.polito.it>
2018-07-12 18:51:39 +0200

    playmusic update

--- client/play_music.py
+++ client/play_music.py
@@ -23,7 +23,7 @@ if __name__ == '__main__':
     for mp3 in mp3s:
         pygame.mixer.init()
         print('carico: ' + mp3)
-        #pygame.mixer.music.load(str(mp3))
+        pygame.mixer.music.load(str(mp3))
         print('riproduco: ' + mp3)
         pygame.mixer.music.play()
         try:
@@ -33,6 +33,8 @@ if __name__ == '__main__':
             """
             time.sleep(5)
         except KeyboardInterrupt:
+            pass
+        except pygame.error:
             pass
         finally:
             pygame.mixer.stop()
