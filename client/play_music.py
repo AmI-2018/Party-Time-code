0aa16e8e67849e1d0bb60918b0c77f6f37d72815
alessio-zamparelli <alessio.zamparelli@studenti.polito.it>
2018-07-12 19:45:32 +0200

    playmusic update

--- client/play_music.py
+++ client/play_music.py
@@ -3,23 +3,26 @@ import time
 import pygame
 import json
 
+initial = True
 if __name__ == '__main__':
     folder = './playlist'
     abspath = os.path.abspath(folder)
+    mp3s = {}
     while True:
         #print(len(elements) for path, directory, elements in os.walk(abspath))
         #print(len([name for name in os.listdir(folder) if os.path.isfile(name)]))
 
 
         #print(len(os.listdir(abspath)))
-        while len(os.listdir(abspath)) == 0:
-            print('non ho file')
-            time.sleep(1)
-        print('attendere 10 secondi')
-        time.sleep(10)
+        if initial==True:
+            while len(os.listdir(abspath)) == 0:
+                print('non ho file')
+                time.sleep(1)
+            print('attendere 10 secondi')
+            time.sleep(10)
         #exit()
         #mp3s = set()
-        mp3s = {}
+
 
 
         #print(abspath)
@@ -44,7 +47,7 @@ if __name__ == '__main__':
                 print('carico: ' + mp3)
                 pygame.mixer.music.load(str(mp3))
                 print('riproduco: ' + mp3 + ' per la ' + str(mp3s[mp3]) + ' volta')
-                mp3s[mp3] = mp3s[mp3] + 1
+                mp3s[mp3]. = mp3s[mp3] + 1
                 pygame.mixer.music.play()
 
 
@@ -52,6 +55,7 @@ if __name__ == '__main__':
                 while pygame.mixer.music.get_busy() == True:
                     time.sleep(0.2)
                 """
+                print('aggiornata:::: ' + str(mp3s[mp3]))
                 time.sleep(5)
             except KeyboardInterrupt:
                 pass
@@ -63,6 +67,18 @@ if __name__ == '__main__':
             finally:
                 pygame.mixer.stop()
 
+
+        print('fine ciclo')
+        print('fine ciclo')
+        print('fine ciclo')
+        print('fine ciclo')
+        print('fine ciclo')
+        print('fine ciclo')
+        print('fine ciclo')
+        print('fine ciclo')
+        print('fine ciclo')
+        print('fine ciclo')
+        print(json.dumps(mp3s, indent=4, sort_keys=False))
 
 
 
