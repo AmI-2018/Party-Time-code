fc061db81e73bdad9ab1625f8bbddcabf2089eda
stathisio <sta8is96@hotmail.com>
2018-07-13 22:54:00 +0200

    hue color setting

--- server/hueOperator.py
+++ server/hueOperator.py
@@ -1,5 +1,7 @@
 import ssdpDiscover as ssdp
 import DBoperator as db
+import rest
+import time
 
 """
 !!!!!!IMPORTANT the app need to verify the room names before......
@@ -45,3 +47,33 @@ class hueOperator(object):
         bri = 254 is the maximum brightness
         """
 post_reply = requests.post(SPOTIFY_TOKEN_URL, data=code_payload, headers=headers).json()
+
+
+base_url = 'http://localhost:8000'
+username = 'newdeveloper'
+lights_url = base_url + '/api/' + username + '/lights/'
+all_the_lights = rest.send(url=lights_url)
+kinds = db.getKindsOfMusic()
+if type(all_the_lights) is dict:
+        # iterate over the Hue lights, turn them on with the color loop effect
+        for light in all_the_lights:
+            url_to_call = lights_url + light + '/state'
+            if kinds == "rock":
+            body = '{"on" : true, "hue" : 0}'
+                 elif kinds == "pop":
+                 body = '{"on" : true, "hue" : 25500}'
+                    elif kinds == "jazz":
+                    body = '{"on" : true, "hue" : 46920}'
+            rest.send('PUT', url_to_call, body, {'Content-Type': 'application/json'})
+            # wait 10 seconds...
+            for i in range(0, 10):
+                time.sleep(1)
+                print(10 - i)
+
+            # iterate over the Hue lights and turn them off
+            for light in all_the_lights:
+                url_to_call = lights_url + light + '/state'
+                body = '{ "on" : false }'
+                rest.send('PUT', url_to_call, body, {'Content-Type': 'application/json'})
+        else:
+            print('Error:', all_the_lights[0]['error'])
