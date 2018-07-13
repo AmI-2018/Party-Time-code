fc066ea5947e22ab9ddf448028d3e4cf9b6cc60f
alessio-zamparelli <alessio.zamparelli@studenti.polito.it>
2018-07-13 20:12:44 +0200

    fix

--- server/create_playlist.py
+++ server/create_playlist.py
@@ -9,8 +9,11 @@ from ftplib import FTP
 import os
 import time
 
+
 tot_user_room = DBoperator.countUserInRooms()
-i = 0 
+
+i = 0
+
 """risultato in elements
 {
     "bagno": {
@@ -35,7 +38,7 @@ for (room2, dict2) in elements.items():
     for (kind2, count2) in dict2.items():
         count3 = count2 * 10 / totals[room2]
         dict2.update({kind2: int(count3)})
-#print(elements)
+print(elements)
 
 """
 for a in tot_user_room:
@@ -104,112 +107,80 @@ for room3, dic in elements.items():
     print()
 
     to_be_sent[room3] = tmp
-    print(to_be_sent)
-print(json.dumps(to_be_sent, indent=4))
+#    print(to_be_sent)
+#print(json.dumps(to_be_sent, indent=4))
 #[print(DBoperator.getIpByName(rom) for to_be_sent)]
 #exit(0)
+"""
+while True:
+    dir = r'C:\Users\lucag\Desktop\SCUOLA\3° ANNO\ambient intelligence\Party-Time-code\server\playlist'
+    filelist = [f for f in os.listdir(dir) if f.endswith(".mp3")]
+    for f in filelist:
+        os.remove(os.path.join(dir, f))
 
-#while True:
-dir = r'C:\Users\lucag\Desktop\SCUOLA\3° ANNO\ambient intelligence\Party-Time-code\server\playlist'
-filelist = [f for f in os.listdir(dir) if f.endswith(".mp3")]
-for f in filelist:
-    os.remove(os.path.join(dir, f))
-
-files_pop = os.listdir(os.path.dirname(r'C:\Users\lucag\Desktop\musica\musica nuova\Calvin Harris\Calvin Harris - Funk Wav Bounces Vol.1 [2017]\\'))
-regex = r"(.mp3)"
-pop_people = 3  #here the effective number taken from the DB
-i = 0
-for f in files_pop:
-    if i == pop_people:
-        break
-    if (re.search(regex, f)):
-        f = random.choice(files_pop)
-        f = os.path.join(r'C:\Users\lucag\Desktop\musica\musica nuova\Calvin Harris\Calvin Harris - Funk Wav Bounces Vol.1 [2017]\\', f)
-        shutil.copy(f, r'C:\Users\lucag\Desktop\SCUOLA\3° ANNO\ambient intelligence\Party-Time-code\server\playlist')
-        i= i+1
-
-
-
-files_rock = os.listdir(os.path.dirname(r'C:\Users\lucag\Desktop\musica\musica nuova\Coldplay\Coldplay - Kaleidoscope EP (2017) (Mp3 320kbps) [Hunter] {786zx}\\'))
-regex = r"(.mp3)"
-rock_people = 5  #here the effective number taken from the DB
-i = 0
-for f in files_rock:
-    if i == rock_people:
-        break
-    if (re.search(regex, f)):
-        f = random.choice(files_rock)
-        f = os.path.join(r'C:\Users\lucag\Desktop\musica\musica nuova\Coldplay\Coldplay - Kaleidoscope EP (2017) (Mp3 320kbps) [Hunter] {786zx}', f)
-        shutil.copy(f, r'C:\Users\lucag\Desktop\SCUOLA\3° ANNO\ambient intelligence\Party-Time-code\server\playlist')
-        i= i+1
-
-
-files_rb = os.listdir(os.path.dirname(r'C:\Users\lucag\Desktop\SCUOLA\3° ANNO\ambient intelligence\Party-Time-code\server\music\rb\\'))
-regex = r"(.mp3)"
-rb_people = 2  #here the effective number taken from the DB
-i = 0
-for f in files_rb:
-    if i == rb_people:
-        break
-    if (re.search(regex, f)):
-        f = random.choice(files_rb)
-        f = os.path.join(r'C:\Users\lucag\Desktop\SCUOLA\3° ANNO\ambient intelligence\Party-Time-code\server\music\rb\\', f)
-        shutil.copy(f, r'C:\Users\lucag\Desktop\SCUOLA\3° ANNO\ambient intelligence\Party-Time-code\server\playlist')
-        i= i+1
+    files_pop = os.listdir(os.path.dirname(r'C:\Users\lucag\Desktop\musica\musica nuova\Calvin Harris\Calvin Harris - Funk Wav Bounces Vol.1 [2017]\\'))
+    regex = r"(.mp3)"
+    pop_people = 3  #here the effective number taken from the DB
+    i = 0
+    for f in files_pop:
+        if i == pop_people:
+            break
+        if (re.search(regex, f)):
+            f = random.choice(files_pop)
+            f = os.path.join(r'C:\Users\lucag\Desktop\musica\musica nuova\Calvin Harris\Calvin Harris - Funk Wav Bounces Vol.1 [2017]\\', f)
+            shutil.copy(f, r'C:\Users\lucag\Desktop\SCUOLA\3° ANNO\ambient intelligence\Party-Time-code\server\playlist')
+            i= i+1
 
 
 
-# ftp settings
-settings = {
-    'ftp': {
-        'url': '192.168.2.34',
-        'username': 'dietpi',
-        'password': 'passwordComplessa',
-        'remote-directory': r'/'
-    }
-}
+    files_rock = os.listdir(os.path.dirname(r'C:\Users\lucag\Desktop\musica\musica nuova\Coldplay\Coldplay - Kaleidoscope EP (2017) (Mp3 320kbps) [Hunter] {786zx}\\'))
+    regex = r"(.mp3)"
+    rock_people = 5  #here the effective number taken from the DB
+    i = 0
+    for f in files_rock:
+        if i == rock_people:
+            break
+        if (re.search(regex, f)):
+            f = random.choice(files_rock)
+            f = os.path.join(r'C:\Users\lucag\Desktop\musica\musica nuova\Coldplay\Coldplay - Kaleidoscope EP (2017) (Mp3 320kbps) [Hunter] {786zx}', f)
+            shutil.copy(f, r'C:\Users\lucag\Desktop\SCUOLA\3° ANNO\ambient intelligence\Party-Time-code\server\playlist')
+            i= i+1
 
-for room,files in to_be_sent:
-    roomIP = DBoperator.getIPbyName(room)
-    
-# local paths
-for music in music_list:
-    # connect and store
-    for f in files:
-        ftp = FTP(settings['ftp']['url'])
-        ftp.login(settings['ftp']['username'], settings['ftp']['password'])
-        ftp.cwd(settings['ftp']['remote-directory'])
-        ftp.storbinary('STOR ' + f, open(paths['local-directory'] + f, 'rb'))
-        ftp.close()
-    time.sleep(5)
 
-""" 
+    files_rb = os.listdir(os.path.dirname(r'C:\Users\lucag\Desktop\SCUOLA\3° ANNO\ambient intelligence\Party-Time-code\server\music\rb\\'))
+    regex = r"(.mp3)"
+    rb_people = 2  #here the effective number taken from the DB
+    i = 0
+    for f in files_rb:
+        if i == rb_people:
+            break
+        if (re.search(regex, f)):
+            f = random.choice(files_rb)
+            f = os.path.join(r'C:\Users\lucag\Desktop\SCUOLA\3° ANNO\ambient intelligence\Party-Time-code\server\music\rb\\', f)
+            shutil.copy(f, r'C:\Users\lucag\Desktop\SCUOLA\3° ANNO\ambient intelligence\Party-Time-code\server\playlist')
+            i= i+1
+"""
+
+for room, files in to_be_sent:
+    roomIP = DBoperator.getIpByName(room)
     # ftp settings
     settings = {
         'ftp': {
-            'url': '192.168.2.34',
+            'url': roomIP,
             'username': 'dietpi',
             'password': 'passwordComplessa',
             'remote-directory': r'/'
         }
     }
+    for music in musi_list:
+        # local paths
 
-    # local paths
-    paths = {
-        'local-directory': r'playlist/'
-    }
-    print(paths['local-directory'])
+        # connect and store
+        for f in files:
+            ftp = FTP(settings['ftp']['url'])
+            ftp.login(settings['ftp']['username'], settings['ftp']['password'])
+            ftp.cwd(settings['ftp']['remote-directory'])
+            ftp.storbinary('STOR ', open(f), 'rb')
+            ftp.close()
 
-    # list of local files
-    files = os.listdir(paths['local-directory'])
-    print(files)
-
-    # connect and store
-    for f in files:
-        ftp = FTP(settings['ftp']['url'])
-        ftp.login(settings['ftp']['username'], settings['ftp']['password'])
-        ftp.cwd(settings['ftp']['remote-directory'])
-        ftp.storbinary('STOR ' + f, open(paths['local-directory'] + f, 'rb'))
-        ftp.close()
-"""
-print()
+            time.sleep(5)
